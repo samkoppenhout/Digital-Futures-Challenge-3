cebe47a6841cef7c3d4856dff6c0f80a955ea8ac
Sam K <samkoppenhout@live.com>
2024-05-13 21:33:05 +0100

    Adding production code to pass new tests

--- src/main/java/com/digitalfutures/application/DataValidator.java
+++ src/main/java/com/digitalfutures/application/DataValidator.java
@@ -1,6 +1,30 @@
 package com.digitalfutures.application;
 
+import java.util.regex.*;
+
 public class DataValidator {
+    static final String phoneNumberRegex = "^(?:(?:\\+|00)44|0)7\\d{9}$";
+    static final Pattern phoneNumberPattern = Pattern.compile(phoneNumberRegex);
+
+    static final String emailRegex = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b";
+    static final Pattern emailPattern = Pattern.compile(emailRegex);
+
+    private static String removeSpaces(String string) {
+        return string.replaceAll(" ", "");
+    }
+    private static String phoneNumberRegEx(String phoneNumber) {
+        Matcher matcher = phoneNumberPattern.matcher(removeSpaces(phoneNumber));
+        if (matcher.matches()) {
+            return removeSpaces(phoneNumber);
+        } else { throw new IllegalArgumentException("Phone Number Invalid"); }
+    }
+    private static String emailRegex(String email) {
+        Matcher matcher = emailPattern.matcher(email);
+        if (matcher.matches()) {
+            return email;
+        } else { throw new IllegalArgumentException("Phone Number Invalid"); }
+    }
+
     static String checkName(String name) {
         if (name != null) {
             return name;
@@ -8,7 +32,12 @@ public class DataValidator {
     }
     static String checkPhoneNumber(String phoneNumber) {
         if (phoneNumber != null) {
-            return phoneNumber;
+            return phoneNumberRegEx(phoneNumber);
         } else {throw new IllegalArgumentException("Phone Number Invalid"); }
+    }
+    static String checkEmail(String email) {
+        if (email != null) {
+            return emailRegex(email);
+        } else {throw new IllegalArgumentException("Email Invalid"); }
     }
 }
--- src/test/java/com/digitalfutures/application/DataValidatorTest.java
+++ src/test/java/com/digitalfutures/application/DataValidatorTest.java
@@ -37,7 +37,7 @@ public class DataValidatorTest {
         @DisplayName("Check that an error is not thrown if phone number is valid")
         void testValidPhoneNumber(){
             // Arrange
-            String testPhoneNumber = "testPhoneNumber";
+            String testPhoneNumber = "07000000000";
             // Act
             // Assert
             assertDoesNotThrow(() -> DataValidator.checkPhoneNumber(testPhoneNumber));
@@ -50,6 +50,47 @@ public class DataValidatorTest {
             // Act
             // Assert
             assertThrows(IllegalArgumentException.class, () -> DataValidator.checkPhoneNumber(testPhoneNumber));
+        };
+        @Test
+        @DisplayName("Check that an error is thrown if phone number is not in a valid format")
+        void testInvalidPhoneNumber(){
+            // Arrange
+            String testPhoneNumber = "Number";
+            // Act
+            // Assert
+            assertThrows(IllegalArgumentException.class, () -> DataValidator.checkPhoneNumber(testPhoneNumber));
+        }
+    }
+
+    @Nested
+    @DisplayName("Email Tests")
+    class EmailTests {
+        @Test
+        @DisplayName("Check that an error is not thrown if email is valid")
+        void testValidEmail(){
+            // Arrange
+            String testEmail = "testemail@email.co.uk";
+            // Act
+            // Assert
+            assertDoesNotThrow(() -> DataValidator.checkEmail(testEmail));
+        };
+        @Test
+        @DisplayName("Check that an error is thrown if email is null")
+        void testNullPhoneNumber(){
+            // Arrange
+            String testEmail = null;
+            // Act
+            // Assert
+            assertThrows(IllegalArgumentException.class, () -> DataValidator.checkEmail(testEmail));
+        };
+        @Test
+        @DisplayName("Check that an error is thrown if email is not in a valid format")
+        void testInvalidPhoneNumber(){
+            // Arrange
+            String testEmail = "Email";
+            // Act
+            // Assert
+            assertThrows(IllegalArgumentException.class, () -> DataValidator.checkEmail(testEmail));
         };
     }
 }
