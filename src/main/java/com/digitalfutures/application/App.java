3cd40177114c59619321781c0328a23eb672f822
Sam K <samkoppenhout@live.com>
2024-05-20 09:44:56 +0100

    including default data

--- src/main/java/com/digitalfutures/application/App.java
+++ src/main/java/com/digitalfutures/application/App.java
@@ -1,6 +1,7 @@
 package com.digitalfutures.application;
 
 import com.digitalfutures.app.AddressBook;
+import com.digitalfutures.app.Contact;
 import com.digitalfutures.app.UserInterface;
 import com.digitalfutures.app.Util.ConsoleIO;
 
@@ -9,6 +10,16 @@ public class App {
         ConsoleIO consoleIO = new ConsoleIO();
         AddressBook addressBook = new AddressBook();
         UserInterface userInterface = new UserInterface(consoleIO, addressBook);
+        Contact contact1 = new Contact("Walter White", "07000000000", "walterwhite@email.com");
+        Contact contact2 = new Contact("Jesse Pinkman", "07111111111", "jpemail@hotmail.co.uk");
+        Contact contact3 = new Contact("Skyler White", "07222222222", "newmail@gmail.com");
+        try {
+            addressBook.addContact(contact1);
+            addressBook.addContact(contact2);
+            addressBook.addContact(contact3);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
         userInterface.start();
     }
 }
